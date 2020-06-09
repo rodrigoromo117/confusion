4645e124a61a2fc5ae5d9be81e4c34915f7884d8
Rodrigo Romo <rodrigo.romo@gmail.com>
2020-06-08 19:57:04 -0500

    npm scripts

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+
+    $(document).ready(function(){   
+    $("#reservebtn").click(function(){
+        $("#reservemodal").modal();   
+        });         
+    });
+
+
+
+    $(document).ready(function(){   
+    $("#loginbtn").click(function(){
+        $("#loginmodal").modal();   
+        });         
+    });
